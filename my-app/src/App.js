763ae0904439ba5888114db3a0cb86fdd4d39f2d
leon <leon.waga@student.moringaschool.comgit config --global user.name leongit config --global user.name leon>
2023-08-21 15:12:51 +0300

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [transactions, setTransactions]= useState([])
+  const [search, setSearch] = useState("")
+  useEffect(() => {
+  fetch(`http://localhost:8001/transactions`)
+  .then((response) => response.json())
+  .then((transaction) => setTransactions(transaction))
+  console.log({transactions});
+}, [search]);
+  
+function addNewTransactions(newForm) {
+  setTransactions(transactions => [...transactions, newForm])
+
+  const servers = {
+    method: 'POST',
+    Headers: { 'content-type': 'application/json' },
+    
+    body: JSON.stringify(newForm)
+  }
+  fetch(`http://localhost:8001/transactions`, servers)
+  .then((response) => response.json())
+  .then((newForm) => console.log(newForm))
 }
 
+  return(
+    <div>
+      <div className="header-container">
+        <h2>The Royal Bank Of Flatiron</h2>
+      </div>
+      <div>
+        <Filter searching={searching} />
+        </div>
+      <div>
+        <NewTransactionForm onTransactionSubmit={addNewTransactions} />
+      </div>
+    </div> 
+  )
+  }
+
+  export default App;
+  
+
+}
 export default App;
